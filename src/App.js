c4946fbc65e70cd6a274b372ce8521c0027f6e7d
ASUS <Yehor Kholodiuk>
2022-05-20 16:53:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ function App() {
     const [name,setName] = useState('')
     const addToList = () => {
     setCount([...count,name])
+        setName('')
+    }
+
+    const deleteItem = (q) => {
+      const updatedList = count.filter( el => el !== q)
+        setCount(updatedList)
     }
   const minus = () =>{
   setCount([...count,Math.random()])
   }
   const plus = () =>{
   setCount([...count,Math.random()])
-  }
+ }
   return (
     <div className="App">
- <button onClick={minus}>minus</button>
+        <button onClick={minus}>minus</button>
       <ul>
-        {count.map(el => <li key={el}>{el}</li>)}
+        {count.map(el => <li key={el}>{el} <button onClick={() => deleteItem(el)}>Delete</button></li>)}
       </ul>
-      <button onClick={plus}>plus</button>
+        <button onClick={plus}>plus</button>
         <input value={name} onChange = {e => setName(e.target.value)}/>
         <button onClick={addToList}>Add to List</button>
     </div>
